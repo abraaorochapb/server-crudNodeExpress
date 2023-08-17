911a52fde6e5f45f49749f72516e4f620fc69bae
abraaorochapb <abraaorochapb@gmail.com>
2023-08-17 15:55:12 -0300

    Adição de um metodo post com um formulario simples

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
-//CRUD - Create, Read, Update, Delete
-//        POST   GET     PUT   DELETE
-
-// https://meuapp.com <- GET -> acesso a minha aplicação
-// https://meuapp.com/contato <- GET -> acesso aos contatos
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
   console.log('http://localhost:3000/')
 })
 
+app.get('/', (req, res) => {
+  res.send(`<form action="/" method="POST">
+    <label for="email">Email:</label>
+    <input type="text" name="email">
+    <input type="submit">
+  </form>`);
+})
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send(`Email enviado : ${req.body.email}`);
+})
+
+app.get('/testes/:id?', (req, res) => {
+  console.log(req.params)
+  res.send('teste')
+})
